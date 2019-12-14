1b8baf9bbfb1a24b31894ad3f2c794ce0289bb57
Pierluigi Galdi <pigi.galdi@gmail.com>
2019-12-14 12:51:19 +0100

    Add didLongPressHandler in PKView

--- PockKit/Sources/PKView.swift
+++ PockKit/Sources/PKView.swift
@@ -14,11 +14,22 @@ open class PKView: NSView {
     
     /// Core
     private var initialPosition: NSPoint?
+    private var didCallLongPressHandler: Bool = false
+    
+    /**
+    Override this property to define custom long-press delay.
+    */
+    open var longPressDelay: TimeInterval = 1.5
     
     /**
     Override this function to define an action for user's tap.
     */
     open func didTapHandler()        { /***/ }
+    
+    /**
+    Override this function to define an action for user's long press.
+    */
+    open func didLongPressHandler() { /***/ }
     
     /**
      Override this function to define an action for user's left swipe.
@@ -30,7 +41,13 @@ open class PKView: NSView {
      */
     open func didSwipeRightHandler() { /***/ }
     
-    /// Override
+    // MARK: Private handlers
+    @objc private func _didLongPress() {
+        self.didCallLongPressHandler = true
+        didLongPressHandler()
+    }
+    
+    // MARK: Overrides
     override open func touchesBegan(with event: NSEvent) {
         /// Touches began
         super.touchesBegan(with: event)
@@ -41,20 +58,23 @@ open class PKView: NSView {
         /// Check if location is in self
         if self.frame.contains(location) {
             self.initialPosition = location
+            self.didCallLongPressHandler = false
+            self.perform(#selector(_didLongPress), with: nil, afterDelay: longPressDelay)
         }
     }
     
-    /// Override
     override open func touchesEnded(with event: NSEvent) {
+        /// Cancel long press handler, if needed
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(_didLongPress), object: nil)
         /// Touches ended
         super.touchesEnded(with: event)
         /// Get touch
-        guard let touch = event.allTouches().first else { return }
+        guard didCallLongPressHandler == false, let touch = event.allTouches().first else { return }
         /// Get touch location
         let location = touch.location(in: self.superview)
         /// Check if location is in self
         if self.frame.contains(location) {
-            /// Check
+            /// Check location
             if location.x < initialPosition?.x ?? location.x {
                 didSwipeLeftHandler()
             }else if location.x > initialPosition?.x ?? location.x {
